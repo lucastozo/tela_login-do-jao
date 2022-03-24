ac236571a5f4fca24e537251a7b45aa6c2edd906
lucastozo <102305949+lucastozo@users.noreply.github.com>
2022-03-24 10:16:00 -0400

    Add files via upload

---
+++ Aula1/Aula1/CalculadoraWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aula1
+{
+    /// <summary>
+    /// Lógica interna para CalculadoraWindow.xaml
+    /// </summary>
+    public partial class CalculadoraWindow : Window
+    {
+        public CalculadoraWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            double peso = Convert.ToDouble(txtPeso.Text);
+            double altura = Convert.ToDouble(txtAltura.Text);
+            double resultado = peso / (altura * altura);
+            MessageBox.Show("O IMC calculado é: " + resultado);
+
+            if (resultado < 18.5)
+            {
+                MessageBox.Show("Classificação: Abaixo do peso");
+            }
+            else if ((resultado >= 18.5) && (resultado <= 24.9))
+            {
+                MessageBox.Show("Classificação: Peso normal");
+            }
+            else if ((resultado >= 25) && (resultado <= 29.9))
+            {
+                MessageBox.Show("Classificação: Sobrepeso");
+            }
+            else if ((resultado >= 30) && (resultado <= 34.9))
+            {
+                MessageBox.Show("Classificação: Obesidade grau 1");
+            }
+            else if ((resultado >= 35) && (resultado <= 39.9))
+            {
+                MessageBox.Show("Classificação: Obesidade grau 2");
+            }
+            else if (resultado >= 40)
+            {
+                MessageBox.Show("Classificação: Obesidade mórbida");
+            }
+        }
+    }
+}
---
+++ Aula1/Aula1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aula1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Password;
+
+            if((usuario == "lucas") && (senha == "123"))
+            {
+                MessageBox.Show("Acertou");
+                CalculadoraWindow calculadora = new CalculadoraWindow();
+                this.Close();
+                calculadora.Show();
+                
+            }
+            else
+            {
+                MessageBox.Show("Errou");
+            }
+        }
+
+        private void btFechar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
